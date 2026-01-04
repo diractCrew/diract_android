e710b32f7a829f5e853536908d6316918300e718
baekcii <bsy8470@naver.com>
2026-01-04 16:41:00 +0900

    chore: MVVM 아키텍처 기반 패키지 구조 생성

--- app/src/main/java/com/baek/diract/presentation/main/MainActivity.kt
+++ app/src/main/java/com/baek/diract/presentation/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.baek.diract
+package com.baek.diract.presentation.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.baek.diract.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
