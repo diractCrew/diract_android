6c8a33ee90d81529877f28c001c6dc38b89ff60c
baekcii <bsy8470@naver.com>
2026-01-04 18:21:00 +0900

    chore: 불필요한 import 제거

--- app/src/main/java/com/baek/diract/presentation/mypage/MyPageFragment.kt
+++ app/src/main/java/com/baek/diract/presentation/mypage/MyPageFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.baek.diract.R
 import com.baek.diract.databinding.FragmentMyPageBinding
 
 class MyPageFragment : Fragment() {
