f520ef572d4cfa769f60470a82ed70d5c0e784a8
baekcii <bsy8470@naver.com>
2026-01-04 18:12:10 +0900

    chore(bottom-nav): 바텀 탭 화면 기본 구조 구성

--- app/src/main/java/com/baek/diract/presentation/inbox/InboxFragment.kt
+++ app/src/main/java/com/baek/diract/presentation/inbox/InboxFragment.kt
@@ -6,14 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.baek.diract.R
+import com.baek.diract.databinding.FragmentInboxBinding
 
 class InboxFragment : Fragment() {
+    private var _binding: FragmentInboxBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inbox, container, false)
+    ): View {
+        _binding = FragmentInboxBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //TODO: 기능 구현
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/baek/diract/presentation/mypage/MyPageFragment.kt
+++ app/src/main/java/com/baek/diract/presentation/mypage/MyPageFragment.kt
@@ -6,14 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.baek.diract.R
+import com.baek.diract.databinding.FragmentMyPageBinding
 
 class MyPageFragment : Fragment() {
+    private var _binding: FragmentMyPageBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
+    ): View {
+
+        _binding = FragmentMyPageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //TODO: 기능 구현
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
