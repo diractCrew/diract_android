5dd6fc920d79dc6b301a04759d33465374c11ed7
baekcii <bsy8470@naver.com>
2026-01-13 05:06:14 +0900

    design: 앱 전역에 다크모드 강제 적용

--- app/src/main/java/com/baek/diract/DiractApplication.kt
+++ app/src/main/java/com/baek/diract/DiractApplication.kt
@@ -1,7 +1,14 @@
 package com.baek.diract
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class DiractApplication : Application()
+class DiractApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        //다크모드 강제적용
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+    }
+}
