da10209c2e89d070907a9b21fbf8bc9e943049d2
baekcii <bsy8470@naver.com>
2026-01-16 11:16:52 +0900

    chore: glide 및 영상 압축관련 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.swiperefreshlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -73,4 +74,13 @@ dependencies {
     //hilt
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
+
+    //glide
+    implementation(libs.glide)
+    ksp(libs.glide.ksp)
+
+    //media3 transformer
+    implementation(libs.media3.transformer)
+    implementation(libs.media3.effect)
+    implementation(libs.media3.common)
 }
