31e4f912c7a6d5c5bfe6cd4bc8ad2feb48de0e8a
baekcii <bsy8470@naver.com>
2026-01-22 04:53:05 +0900

    refactor: formatter 이름 수정 및 밀리초 단위 변환 함수 구현

--- app/src/main/java/com/baek/diract/presentation/common/Formatter.kt
+++ app/src/main/java/com/baek/diract/presentation/common/Formatter.kt
@@ -1,8 +1,8 @@
 package com.baek.diract.presentation.common
 
 import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 object Formatter {
     private val DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일")
@@ -11,10 +11,23 @@ object Formatter {
         return this.format(DATE_FORMATTER)
     }
 
-    fun Double.toUiString(): String {
+    fun Double.toTimeString(): String {
         val totalSeconds = this.toInt()
         val minutes = totalSeconds / 60
         val secs = totalSeconds % 60
         return String.format("%02d:%02d", minutes, secs)
+    }
+
+    fun Long.toTimeString(): String {
+        val totalSeconds = this / 1000
+        val hours = totalSeconds / 3600
+        val minutes = (totalSeconds % 3600) / 60
+        val seconds = totalSeconds % 60
+
+        return if (hours > 0) {
+            String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, seconds)
+        } else {
+            String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds)
+        }
     }
 }
